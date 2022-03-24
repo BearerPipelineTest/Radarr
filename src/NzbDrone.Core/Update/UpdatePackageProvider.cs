Prowlarr/Prowlarr

DbType param on update check

src/NzbDrone.Core/Update/UpdatePackageProvider.cs
@@ -5,6 +5,7 @@ using NzbDrone.Common.Cloud;
 using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Http;
 using NzbDrone.Core.Analytics;
+using NzbDrone.Core.Datastore;
 
 namespace NzbDrone.Core.Update
 {
@@ -20,13 +21,15 @@ namespace NzbDrone.Core.Update
         private readonly IHttpRequestBuilderFactory _requestBuilder;
         private readonly IPlatformInfo _platformInfo;
         private readonly IAnalyticsService _analyticsService;
+        private readonly IMainDatabase _mainDatabase;
 
-        public UpdatePackageProvider(IHttpClient httpClient, IProwlarrCloudRequestBuilder requestBuilder, IAnalyticsService analyticsService, IPlatformInfo platformInfo)
+        public UpdatePackageProvider(IHttpClient httpClient, IProwlarrCloudRequestBuilder requestBuilder, IAnalyticsService analyticsService, IPlatformInfo platformInfo, IMainDatabase mainDatabase)
         {
             _platformInfo = platformInfo;
             _analyticsService = analyticsService;
             _requestBuilder = requestBuilder.Services;
             _httpClient = httpClient;
+            _mainDatabase = mainDatabase;
         }
 
         public UpdatePackage GetLatestUpdate(string branch, Version currentVersion)
@@ -38,6 +41,7 @@ namespace NzbDrone.Core.Update
                                          .AddQueryParam("arch", RuntimeInformation.OSArchitecture)
                                          .AddQueryParam("runtime", PlatformInfo.Platform.ToString().ToLowerInvariant())
                                          .AddQueryParam("runtimeVer", _platformInfo.Version)
+                                         .AddQueryParam("dbType", _mainDatabase.DatabaseType)
                                          .SetSegment("branch", branch);
 
             if (_analyticsService.IsEnabled)
@@ -65,6 +69,7 @@ namespace NzbDrone.Core.Update
                                          .AddQueryParam("arch", RuntimeInformation.OSArchitecture)
                                          .AddQueryParam("runtime", PlatformInfo.Platform.ToString().ToLowerInvariant())
                                          .AddQueryParam("runtimeVer", _platformInfo.Version)
+                                         .AddQueryParam("dbType", _mainDatabase.DatabaseType)
                                          .SetSegment("branch", branch);
 
             if (previousVersion != null && previousVersion != currentVersion)

BearerPipelineTest/Radarr

(cherry picked from commit 9cb04466c15ca3080d6e2b121d141d3a0442a0cc)

src/NzbDrone.Core/Update/UpdatePackageProvider.cs
@@ -5,6 +5,7 @@ using NzbDrone.Common.Cloud;
 using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Http;
 using NzbDrone.Core.Analytics;
+using NzbDrone.Core.Datastore;
 
 namespace NzbDrone.Core.Update
 {
@@ -20,13 +21,15 @@ namespace NzbDrone.Core.Update
         private readonly IHttpRequestBuilderFactory _requestBuilder;
         private readonly IPlatformInfo _platformInfo;
         private readonly IAnalyticsService _analyticsService;
+        private readonly IMainDatabase _mainDatabase;
 
-        public UpdatePackageProvider(IHttpClient httpClient, IRadarrCloudRequestBuilder requestBuilder, IAnalyticsService analyticsService, IPlatformInfo platformInfo)
+        public UpdatePackageProvider(IHttpClient httpClient, IRadarrCloudRequestBuilder requestBuilder, IAnalyticsService analyticsService, IPlatformInfo platformInfo, IMainDatabase mainDatabase)
         {
             _platformInfo = platformInfo;
             _analyticsService = analyticsService;
             _requestBuilder = requestBuilder.Services;
             _httpClient = httpClient;
+            _mainDatabase = mainDatabase;
         }
 
         public UpdatePackage GetLatestUpdate(string branch, Version currentVersion)
@@ -38,6 +41,7 @@ namespace NzbDrone.Core.Update
                                          .AddQueryParam("arch", RuntimeInformation.OSArchitecture)
                                          .AddQueryParam("runtime", PlatformInfo.Platform.ToString().ToLowerInvariant())
                                          .AddQueryParam("runtimeVer", _platformInfo.Version)
+                                         .AddQueryParam("dbType", _mainDatabase.DatabaseType)
                                          .SetSegment("branch", branch);
 
             if (_analyticsService.IsEnabled)